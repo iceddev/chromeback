b999431a8ca9224dbe885e3287ad9d9c96bbbc2a
Brett Warner <bkwarner22@gmail.com>
2015-05-22 12:36:56 -0700

    initial implementation and docs

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # chromeback
 Mapping of Chrome APIs to node-style callbacks
+
+## Usage
+```
+var chromeApi = require('chromeback');
+
+chromeApi.storage.sync.get('myItem', function(err, res){
+  if(err){
+    console.log('You would have had to check chrome.runtime.lastError to see this before!');
+  }
+  return res;
+}
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var _ = require('lodash');
+
+function buildApi(newApi, chromeApi){
+  // Recursively goes down the chrome Api
+  // First we get all the keys for the level we're on
+  var keys = _.keysIn(chromeApi);
+
+  _.forEach(keys, function(key){
+    // then we loop over them and assign them
+    // this first one is just a safety in case of non-objects.
+    newApi[key] = chromeApi[key];
+
+    if(_.isObject(chromeApi[key])){
+      // If we hit an object we want to go down inside of it
+      // Cloning it so we recursively call the function again
+      newApi[key] = buildApi({}, chromeApi[key]);
+    }
+
+    if(_.isFunction(chromeApi[key])){
+      // Aha! A function here the real fun begins
+      newApi[key] = _.restParam(function(args){
+        var lastElement = _.last(args);
+
+        if(_.isFunction(lastElement)){
+          // If the last element of the arguments array is a function we assume it's a callback and remove it
+          var callback = lastElement;
+          args.pop();
+
+          // Then we replace it with our own function which handles the error cases
+          var replacementCallback = _.restParam(function(res){
+            if(chrome.runtime.lastError){
+              return callback(chrome.runtime.lastError);
+            } else {
+              return callback.apply(null, [null].concat(res));
+            }
+          });
+          // Add our new function to the end of the arguments we're going to call the api with.
+          args.push(replacementCallback);
+          // Then call it with all of the original arguments plus our new callback(Which eventually calls the old callback)
+          chromeApi[key].apply(chromeApi, args);
+        } else {
+          // Callbacks are optional in a lot of cases. If we weren't given one we just call the api normally.
+          return chromeApi[key].apply(chromeApi, args);
+        }
+      });
+    }
+  });
+
+  return newApi;
+}
+
+var createdApi = buildApi({}, chrome);
+
+module.exports = createdApi;
